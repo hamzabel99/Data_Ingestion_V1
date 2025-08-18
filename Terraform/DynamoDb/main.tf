a18156e3b13bbde62f01df376e3fa9cdf537005c
Hamza Belabbes <hamza.belabbes.vt2i@fmail.com>
2025-08-18 23:14:01 +0200

    Add new DynamoDB table

--- Terraform/DynamoDb/main.tf
+++ Terraform/DynamoDb/main.tf
@@ -1,11 +1,21 @@
-resource "aws_dynamodb_table" "example" {
-    name             = "workflow_metadata"
-    hash_key         = "S3_prefix"
-    billing_mode     = "PAY_PER_REQUEST"
+resource "aws_dynamodb_table" "workflow_metadata_table" {
+  name         = "workflow_metadata"
+  hash_key     = "s3_prefix"
+  billing_mode = "PAY_PER_REQUEST"
 
-    attribute {
-    name = "S3_prefix"
+  attribute {
+    name = "s3_prefix"
     type = "S"
   }
 }
 
+resource "aws_dynamodb_table" "workflow_statut_table" {
+  name         = "workflow_statut"
+  hash_key     = "s3_prefix"
+  billing_mode = "PAY_PER_REQUEST"
+
+  attribute {
+    name = "s3_prefix"
+    type = "S"
+  }
+}
