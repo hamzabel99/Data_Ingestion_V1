993da32d8422a6ec124b052c6daff651cc6bb886
Hamza Belabbes <hamza.belabbes.vt2i@fmail.com>
2025-08-18 23:16:34 +0200

    Add S3 post process S3 bucket

--- Terraform/S3Bucket/main.tf
+++ Terraform/S3Bucket/main.tf
@@ -1,3 +1,7 @@
 resource "aws_s3_bucket" "preprocess_bucket" {
   bucket = var.preprocess_bucket_name
 }
+
+resource "aws_s3_bucket" "postprocess_bucket" {
+  bucket = var.postprocess_bucket_name
+}
--- Terraform/S3Bucket/variables.tf
+++ Terraform/S3Bucket/variables.tf
@@ -1,4 +1,9 @@
 variable "preprocess_bucket_name" {
-    description = "Name of the bucket for the preprocessed data"
-    type= string
+  description = "Name of the bucket for the preprocessed data"
+  type        = string
+}
+
+variable "postprocess_bucket_name" {
+  description = "Name of the bucket for the postprocess data"
+  type        = string
 }
